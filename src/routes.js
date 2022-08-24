8c6632e13e12e261dcf18fb1d2c907000b6bacc3
Samuel <samuelwill.ds@gmail.com>
2022-08-24 20:45:57 -0300

    corrigindo o model das marcas

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,13 @@ const UserController = require('./controllers/UserController')
 const CarsController = require('./controllers/CarsController')
 const MarcasController = require('./controllers/MarcasController')
 const CarroceriasController = require('./controllers/CarroceriasController')
-
-
-
 const routes = express.Router()
-
 routes.get('/user', UserController.index);
 routes.get('/cars', CarsController.index);
 routes.post('/user', UserController.store);
 routes.post('/marcas/:marca_id/cars', MarcasController.store);
 routes.get('/marcas/:marca_id/cars', MarcasController.index);
 routes.get('/carrocerias/:carroceria_id/cars', CarroceriasController.index );
-
 routes.post('/carrocerias/:carroceria_id/cars', CarroceriasController.store );
 
 
